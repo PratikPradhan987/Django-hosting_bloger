78b8621cdbebf95a6aa262640d3eab90fa0c1960
Pratik <pp3757634@gmail.com>
2024-03-16 11:30:39 +0530

    initila changes

--- blog/urls.py
+++ blog/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from blogs.views import *
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("",home ),
-
+    path("",landing_page ),    
 ]

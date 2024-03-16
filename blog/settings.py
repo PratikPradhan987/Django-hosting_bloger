ebec9a7a0447ccb70d2729a10fb9ca88b263a1a0
Pratik <pp3757634@gmail.com>
2024-03-16 11:17:28 +0530

    dsa

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-x8@c9@2)vqx@0+kir79vk2ez_fua+&u9)!fb*4h-6gbmtndu-9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app' ,'.now.sh']
 
 # Application definition
 
@@ -73,10 +73,6 @@ WSGI_APPLICATION = 'blog.wsgi.app'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
 }
 
 
